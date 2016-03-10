4a84276dcd9a39532051a3223ad0f4b3fdaadf8a
Sanjay Nair <sanjayhnair@gmail.com>
2016-03-10 12:09:16 -0500

    Finished 4.3. Wrote function to open and validate PAK files

--- Module4/Source.c
+++ Module4/Source.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "file.h"
+
+#define MAX_FILES_IN_PACK 2048
+
+typedef struct {
+	char Magic[4];
+	int DirectoryOffset;
+	int DirectoryLength;
+} dpackheader_t;
+
+typedef struct {
+	char FileName[56];
+	int FilePosition;
+	int FileLength;
+} dpackfile_t;
+
+typedef struct {
+	char FileName[64];
+	int FilePosition;
+	int FileLength;
+} packfile_t;
+
+typedef struct {
+	char PackName[128];
+	int PackHandle;
+	int NumberOfFiles;
+	packfile_t* PackFiles;
+} pack_t;
 
 char* va(char* format, ...) {
 	// Used to walk through variable arg list
@@ -23,35 +51,57 @@ char* va(char* format, ...) {
 
 }
 
+pack_t* COM_LoadPackFile(char *pakFile) {
+	int pakSize;
+	int pakHandle = Sys_FileOpenRead(pakFile, &pakSize);
+
+	if (pakHandle == -1) {
+		int a = 5; a;
+	}
+
+	dpackheader_t pakHeader;
+	Sys_FileRead(pakHandle, &pakHeader, sizeof(pakHeader));
+
+	// Check magic number
+	if (pakHeader.Magic[0] != 'P' || pakHeader.Magic[1] != 'A' || pakHeader.Magic[2] != 'C' || pakHeader.Magic[3] != 'K') {
+		int a = 5; a;
+	}
+
+
+	// Get the number of files that exist in the PAK file
+	int numPackFiles = pakHeader.DirectoryLength / sizeof(dpackfile_t);
+
+	// Hold the opened file in memory
+	packfile_t* filesInMemory = malloc(numPackFiles * sizeof(packfile_t));
+
+	Sys_FileSeek(pakHandle, pakHeader.DirectoryOffset);
+
+	dpackfile_t tempPackFile[MAX_FILES_IN_PACK];
+	Sys_FileRead(pakHandle, (void*)tempPackFile, pakHeader.DirectoryLength);
+
+	for (int i = 0; i < numPackFiles; ++i) {
+		strcpy_s(filesInMemory[i].FileName, 56, tempPackFile[i].FileName);
+		filesInMemory[i].FileLength = tempPackFile[i].FileLength;
+		filesInMemory[i].FilePosition = tempPackFile[i].FilePosition;
+	}
+
+	// Pack up files and info into a pack object
+	pack_t* Pak0 = (void*)malloc(sizeof(pack_t));
+	strcpy_s(Pak0->PackName, 64, pakFile);
+	Pak0->PackHandle = pakHandle;
+	Pak0->NumberOfFiles = numPackFiles;
+	Pak0->PackFiles = filesInMemory;
+
+	return Pak0;
+}
+
 int main() {
 
-	int size;
+	
+	pack_t* Pak0 = COM_LoadPackFile("../../id1/PAK0.PAK");
+	pack_t* Pak1 = COM_LoadPackFile("../../id1/PAK1.PAK");
 
-	int fileHandle = Sys_FileOpenRead("../../Source.c", &size);
-
-	if (fileHandle == -1) {
-		int a = 5; a;
-	}
-
-	void* buffer = malloc(size);
-
-	Sys_FileRead(fileHandle, buffer, size);
-
-	int writeFile = Sys_FileOpenWrite("../../Source.out");
-
-	if (writeFile == -1) {
-		int a = 5; a;
-	}
-
-	Sys_FileWrite(writeFile, buffer, size);
-
-	Sys_FileClose(fileHandle);
-	Sys_FileClose(writeFile);
-
-	free(buffer);
-	buffer = NULL;
-
-	// char* colorData = LoadFile("palette.lmp");
+	int a = 5; a;
 
 	return 0;
 }
